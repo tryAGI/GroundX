2d752b37ae5a84533ce90bca36d771a5f49e00b8
Konstantin S. <havendv@gmail.com>
2026-05-24 10:30:50 +0400

    feat: Updated OpenAPI spec (#28)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/GroundX/Generated/GroundX.GroupsClient.GroupList.g.cs
+++ src/libs/GroundX/Generated/GroundX.GroupsClient.GroupList.g.cs
@@ -373,17 +373,15 @@ namespace GroundX
                                 }
                                 catch (global::System.Exception __ex)
                                 {
-                                    throw new global::GroundX.ApiException(
+                                    throw global::GroundX.ApiException.Create(
+                                        statusCode: __response.StatusCode,
                                         message: __content ?? __response.ReasonPhrase ?? string.Empty,
                                         innerException: __ex,
-                                        statusCode: __response.StatusCode)
-                                    {
-                                        ResponseBody = __content,
-                                        ResponseHeaders = global::System.Linq.Enumerable.ToDictionary(
+                                        responseBody: __content,
+                                        responseHeaders: global::System.Linq.Enumerable.ToDictionary(
                                             __response.Headers,
                                             h => h.Key,
-                                            h => h.Value),
-                                    };
+                                            h => h.Value));
                                 }
                             }
                             else
@@ -420,17 +418,15 @@ namespace GroundX
                                     {
                                     }
 
-                                    throw new global::GroundX.ApiException(
+                                    throw global::GroundX.ApiException.Create(
+                                        statusCode: __response.StatusCode,
                                         message: __content ?? __response.ReasonPhrase ?? string.Empty,
                                         innerException: __ex,
-                                        statusCode: __response.StatusCode)
-                                    {
-                                        ResponseBody = __content,
-                                        ResponseHeaders = global::System.Linq.Enumerable.ToDictionary(
+                                        responseBody: __content,
+                                        responseHeaders: global::System.Linq.Enumerable.ToDictionary(
                                             __response.Headers,
                                             h => h.Key,
-                                            h => h.Value),
-                                    };
+                                            h => h.Value));
                                 }
                             }
 
